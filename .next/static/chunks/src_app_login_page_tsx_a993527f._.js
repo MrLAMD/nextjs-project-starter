9293a1eca1e4e78dc067a6919681ca9f9075eb35
MrLAMD <likhonadakuse816@gmail.com>
2025-07-21 14:48:40 +0200

    Add .next/static/chunks/src_app_login_page_tsx_a993527f._.js 1753102118811

---
+++ .next/static/chunks/src_app_login_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_e4c3e3b0._.js",
+  "static/chunks/src_0cb3eb75._.js"
+],
+    source: "dynamic"
+});
